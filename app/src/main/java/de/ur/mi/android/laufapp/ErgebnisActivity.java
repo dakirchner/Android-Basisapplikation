9cb5e29ab5de80ff4472d3cbc73767d7f5c55bee
dakirchner <dakirchner@kirk-make-it.de>
2020-11-18 19:58:20 +0100

    commit

---
+++ app/src/main/java/de/ur/mi/android/laufapp/ErgebnisActivity.java
@@ -0,0 +1,16 @@
+package de.ur.mi.android.laufapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class ErgebnisActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
--- app/src/main/java/de/ur/mi/android/laufapp/StartActivity.java
+++ app/src/main/java/de/ur/mi/android/laufapp/StartActivity.java
@@ -2,15 +2,45 @@ package de.ur.mi.android.laufapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class StartActivity extends Activity {
+public class StartActivity extends Activity implements View.OnClickListener{
+
+    private TextView mainText;
+    private EditText distance;
+    private EditText time;
+    private EditText breakTime;
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         // Starten Sie hier mit Ihrer Implementierung
+        initUI();
+    }
+
+    private void initUI() {
+        setContentView(R.layout.activity_main);
+
+        //mainText = (TextView) findViewById(R.id.main);
+        //mainText.setText(R.string.main_text);
+
+        distance = (EditText)  findViewById(R.id.distance);
+        time = (EditText)  findViewById(R.id.time);
+        breakTime = (EditText)  findViewById(R.id.breakTime);
+
+        button = (Button)  findViewById(R.id.button);
+
+        button.setOnClickListener(this);
 
 
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
